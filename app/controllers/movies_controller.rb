eb9ac3d6e968ede24b6c0451b10e249906db12a4
Cheri <avyishi@gmail.com>
2015-07-05 21:58:17 -0700

    Updated new movie view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,4 +1,5 @@
 class MoviesController < ApplicationController
+  before_action :set_movie, only: [:show,:edit, :update, :destroy]
 
   def index
     @movies = Movie.all
@@ -11,6 +12,28 @@ class MoviesController < ApplicationController
     @movie = Movie.new
   end
 
+   def create
+    @movie = Movie.new
+    @movie.title = params[:title]
+    @movie.director = params[:director]
+    @movie.released = params[:released]
+    
+    if @movie.save
+      redirect_to @movie, notice: 'Movie Added!'
+    else
+      render action: 'new'
+    end
+  end
+
   def edit
+  end
+
+  private
+  def set_movie
+    @movie = Movie.find(params[:id])
+  end
+
+  def movie_params
+    params.require(:movie).permit(:title)
   end
 end

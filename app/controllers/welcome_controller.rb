307ae71dcf885608d908c902e5c0ed8aceb35277
Cheri <avyishi@gmail.com>
2015-07-17 14:51:46 -0700

    Added about page and updated layout

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,6 +3,9 @@ class WelcomeController < ApplicationController
     @movies = Movie.order('Created_at asc')
   end
 
+  def about
+  end
+
   def movie_params
     params.require(:movie).permit(:title, :image)
   end

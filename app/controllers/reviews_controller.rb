882742036b3f4ea0def5dc1e876e81ab6fe28ae2
Cheri <avyishi@gmail.com>
2015-07-24 07:55:01 -0400

    Updated reviews controller

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -3,4 +3,24 @@ class ReviewsController < ApplicationController
   def new
     @review = Review.new
   end
+
+  def create
+    @review = Review.new(review_params)
+  
+    if @review.save
+      redirect_to @movie
+    else
+      render 'new'
+    end
+  end
+
+  def update
+    @review.update(review_params)
+  end
+
+  private
+  
+    def review_params
+      params.require(:review).permit(:comment)
+    end
 end
